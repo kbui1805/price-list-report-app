42e7894142f4bb9c06921e6db33ff205c2e5e379
Khanh Bui <khanhbui.work@gmail.com>
2022-12-17 21:46:26 -0500

    Inital Commit

---
+++ create-price-list.js
@@ -0,0 +1,9 @@
+const fs = require('fs');
+// const pdfDocument = require('pdfkit');
+const csv = require('csvtojson');
+
+// read csv from data-price
+// create object for each entry
+// create array to hold all product objects
+// read from data item list and create array to hold all item need for price list
+// loop through array item list and create new array to hold all required object items
